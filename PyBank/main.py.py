581810d5fc9c366a00b6ad4fe448fa71011714a3
Nathanhans <nathan.hans@gmail.com>
2023-10-19 23:14:35 -0600

    Fixed the difference formula, was incorrect

--- PyBank/main.py.py
+++ PyBank/main.py.py
@@ -21,6 +21,7 @@ with open(csvpath) as csvfile:
     # Read the header row first (skip this step if there is no header)
     csv_header = next(csvreader)
 
+    #set variables
     Total_months = 0
     Total = 0
     Max_increase = 0
@@ -28,39 +29,58 @@ with open(csvpath) as csvfile:
     Max_increase_month = 0
     Max_decrease_month = 0
     Average_change = 0
+    Previous_row = 0
+    Difference = 0
+    Cml_difference = 0
+
 
     # Read each row of data after the header
     for row in csvreader:
-        print(row)
+      
         #increment months
         Total_months = Total_months+1
-        #add to total
+        
+        #calculate difference
+        if Total_months > 1:
+            Difference = int(row[1]) - Previous_row
+        else:
+            Difference = 0
+        #calculate cumlative differences
+        Cml_difference = Cml_difference + Difference
+        
+
+        
+        #add up total
         Total = Total + int(row[1])
         #conditional to determine min and max
-        if int(row[1]) < Max_decrease:
-            Max_decrease = int(row[1])
+        if Difference < Max_decrease:
+            Max_decrease =Difference
             Max_decrease_month = (row[0])
         else:
             Max_decrease = Max_decrease
         #conditional to determine min and max
-        if int(row[1]) > Max_increase:
-            Max_increase = int(row[1])
+        if Difference > Max_increase:
+            Max_increase = Difference
             Max_increase_month = (row[0])
         else:
-            Max_increase = Max_increase    
-    #calculate average change
-    Average_change = Total/Total_months
+            Max_increase = Max_increase  
+            
+        #Set current value so next loop will reference it as the prior value        
+        Previous_row = int(row[1])
+        
+    #calculate average change, n-1 as the first value has no change
+    Average_difference = Cml_difference/(Total_months-1)
     
     #print requirements
     print(f"Financial Analysis \n---------------------")
     print(f"Total Months: {Total_months}")
     print(f"Total: ${Total}")
-    print(f"Average Change: ${Average_change}")
+    print(f"Average Change: ${Average_difference}")
     print(f"Greatest Increase in Profits: {Max_increase_month} (${Max_increase})")
     print(f"Greatest Decrease in Profits: {Max_decrease_month} (${Max_decrease})")
           
 # Specify the file to write to
-output_path = os.path.join('analysis', 'new.csv')     
+output_path = os.path.join('output', 'new.csv')     
 
           
      # Open the file using "write" mode. Specify the variable to hold the contents.  Had to add newline = '' to avoid skipped rows
@@ -74,6 +94,6 @@ with open(output_path, 'w', newline = '') as csvfile:
                         ,'Greatest Increase','Greatest Decrease Month','Greatest Decrease'])
 
     # Write the second row
-    csvwriter.writerow([Total_months,Total,Average_change, Max_increase_month, Max_increase,Max_decrease_month,Max_decrease])     
+    csvwriter.writerow([Total_months,Total,Average_difference, Max_increase_month, Max_increase,Max_decrease_month,Max_decrease])     
           
           
